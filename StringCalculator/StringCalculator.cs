fac0b51b66c2631f1314442c213d0a3995223b36
ggdol <dolgg0244@gmail.com>
2023-12-13 14:38:00 +0400

    Add project files.

---
+++ StringCalculator/StringCalculator.cs
@@ -0,0 +1,73 @@
+﻿namespace StringCalculatorKata
+{
+    public class StringCalculator
+    {
+        private List<string> delimiters = new() {",", "\n"};
+        public int Add(string numbers)
+        {
+            if (string.IsNullOrWhiteSpace(numbers))
+            {
+
+                return 0;
+            }
+
+            IEnumerable<int> parsedNumbers = ParseNumbers(numbers);
+            ThrowErrorIfAnyNegativeNumbersAreFound(parsedNumbers);
+
+            return parsedNumbers.Where(x => x<= 1000).Sum();
+        }
+
+        private IEnumerable<int> ParseNumbers(string numbers)
+        {
+            numbers = ExtractCustomDelimiter(numbers);
+
+            var parsedNumbers = SplitNumbers(numbers).Select(int.Parse);
+            return parsedNumbers;
+        }
+
+        private static void ThrowErrorIfAnyNegativeNumbersAreFound(IEnumerable<int> parsedNumbers)
+        {
+            var negatives = parsedNumbers.Where(i => i < 0);
+
+            if (negatives.Any())
+                throw new Exception("negatives not allowed: " + string.Join(", ", negatives));
+        }
+
+        private string ExtractCustomDelimiter(string numbers)
+        {
+            if (HasCustomDelimiters(numbers))
+            {
+                var endOfDelimiters = numbers.IndexOf("\n");
+                Console.WriteLine("endOfDelimiters: "+ endOfDelimiters);
+                var fullDelimiterDefinition = numbers.Substring(2, endOfDelimiters - 2);
+                Console.WriteLine("fullDelimiterDefinition: " + fullDelimiterDefinition);
+                if (fullDelimiterDefinition.Length > 1) {
+
+                    delimiters.Add(fullDelimiterDefinition.Trim('[', ']'));
+                    Console.WriteLine("delimiters.Add: " + fullDelimiterDefinition.Substring(1, fullDelimiterDefinition.Length - 2));
+                }
+                else
+                {
+
+                    delimiters.Add(numbers.Substring(2, 1));
+                    Console.WriteLine("delimiters.Add(numbers.Substring(2, 1)): " + numbers.Substring(2, 1));
+                }
+                numbers = numbers.Substring(endOfDelimiters);
+                Console.WriteLine("numbers.Substring(endOfDelimiters): " + numbers.Substring(endOfDelimiters));
+            }
+
+            return numbers;
+        }
+
+        private static bool HasCustomDelimiters(string numbers)
+        {
+            return numbers.StartsWith("//");
+        }
+
+        private string[] SplitNumbers(string numbers)
+        {
+            return numbers.Split(delimiters.ToArray(), StringSplitOptions.RemoveEmptyEntries);
+        }
+
+    }
+}
---
+++ StringCalculatorTests/StringCalculatorKataTests.cs
@@ -0,0 +1,195 @@
+﻿using NUnit.Framework;
+using StringCalculatorKata;
+using static StringCalculatorTests.StringCalculatorKataTests.StringCalculatorTests.Add;
+namespace StringCalculatorTests
+{
+    public class StringCalculatorKataTests
+    {
+
+        //Green bar patterns:
+        //  - One to Many
+        //  - Obvious
+        //  - Backout
+        //  - Learning Test
+        //TDD Gears: Low, Medium, Revers
+        //
+        // Medium gear -> One number + Obvious -> No Numbers +Obvious -> Many numbers + One to many -> Demo Backout +Learning
+
+        [TestFixture]
+        public class StringCalculatorTests
+        {
+            
+            public class Add
+            {
+               
+                public class NoNumbers
+                {
+                    [TestCase(null, 0)]
+                    [TestCase("", 0)]
+                    [TestCase(" ", 0)]
+                    [TestCase("  ", 0)]
+                    public void ShouldReturn0(string numbers, int expected)
+                    {
+                        //Arrange
+                        var sut = CreateStringCalculator();
+                        //Act
+                        var actual = sut.Add(numbers);
+                        //Assert
+                        Assert.AreEqual(expected, actual);
+                    }
+                }
+                public class OneNumber
+                {
+
+                    [TestCase("1", 1)]
+                    [TestCase("15", 15)]
+                    [TestCase("999", 999)]
+                    public void ShouldReturnThatNumber(string numbers, int expected)
+                    {
+                        //Arrange
+                        var sut = CreateStringCalculator();
+                        //Act
+                        var actual = sut.Add(numbers);
+                        //Assert
+                        Assert.AreEqual(expected, actual);
+                    }
+
+                }
+
+
+             
+                public class ManyNumbers
+                {
+
+                    [TestCase("1,2", 3)]
+                    [TestCase("5,15,20", 40)]
+                    [TestCase("999,1,11,2", 1013)]
+                    public void ShouldReturnThoseNumbers(string numbers, int expected)
+                    {
+                        //Arrange
+                        var sut = CreateStringCalculator();
+                        //Act
+                        var actual = sut.Add(numbers);
+                        //Assert
+                        Assert.AreEqual(expected, actual);
+                    }
+                    [TestCase("3,4\n5", 12)]
+                    [TestCase("10\n20\n30\n40", 100)]
+                    [TestCase("5\n5", 10)]
+                    public void SeparatedByNewLine_ShouldReturnTheSumOfThoseNumbers(string numbers, int expected)
+                    {
+                        //Arrange
+                        StringCalculator sut = CreateStringCalculator();
+                        //Act
+                        var actual = sut.Add(numbers);
+                        //Assert
+                        Assert.AreEqual(expected, actual);
+                    }
+                }
+
+
+                public class CustomeDelimiters
+                {
+
+                    [TestCase("//;\n1;2", 3)]
+                    [TestCase("//|\n1|2|3|4", 10)]
+                    public void ShouldReturnThatNumber(string numbers, int expected)
+                    {
+                        //Arrange
+                        var sut = CreateStringCalculator();
+                        //Act
+                        var actual = sut.Add(numbers);
+                        //Assert
+                        Assert.AreEqual(expected, actual);
+                    }
+
+                }
+
+                public class NegativeNumbers
+                {
+                    [TestCase("-1", "negatives not allowed: -1")]
+                    [TestCase("1,-10,2", "negatives not allowed: -10")]
+                    [TestCase("10,20,-30", "negatives not allowed: -30")]
+                    public void OneNegative_ShouldThrowExceptionWithAllNegativesInMessage(string numbers, string expected)
+                    {
+
+                        //Arrange
+                        var sut = CreateStringCalculator(); 
+                        //Act
+                        var exception = Assert.Throws<Exception>(() => sut.Add(numbers));
+                        //Assert
+                        Assert.IsNotNull(exception);
+                        Assert.AreEqual(expected, exception?.Message);
+                    }
+
+                    [TestCase("-1,-5", "negatives not allowed: -1, -5")]
+                    [TestCase("1,-10,-2", "negatives not allowed: -10, -2")]
+                    [TestCase("10,20,-30,40,-50,-100", "negatives not allowed: -30, -50, -100")]
+                    public void MultipleNegatives_ShouldThrowExceptionWithAllNegativesInMessage(string numbers, string expected)
+                    {
+
+                        //Arrange
+                        var sut = CreateStringCalculator();
+                        //Act
+                        var exception = Assert.Throws<Exception>(() => sut.Add(numbers));
+                        //Assert
+                        Assert.AreEqual(expected, exception?.Message);
+                    }
+                }
+
+                public class LargeNumbers {
+
+                    [TestCase("2,1001", 2)]
+                    [TestCase("1,2,1001,3,1002,4,1003,5", 15)]
+                    [TestCase("1000,1", 1001)]
+                    public void ShouldBeIgnored(string numbers, int expected)
+                    {
+
+                        //Arrange
+                        var sut = CreateStringCalculator();
+                        //Act
+                        var actual = sut.Add(numbers);
+                        //Assert
+                        Assert.AreEqual(expected, actual);
+                    }
+                }
+
+
+                public class ArbitraryLengthDelimiter
+                {
+
+                    [TestCase("//[***]\n1***2***3", 6)]
+                    [TestCase("//[&&]\n1&&200&&5", 206)]
+                    public void ShouldBeIgnored(string numbers, int expected)
+                    {
+
+                        //Arrange
+                        var sut = CreateStringCalculator();
+                        //Act
+                        var actual = sut.Add(numbers);
+                        //Assert
+                        Assert.AreEqual(expected, actual);
+                    }
+                }
+
+
+            }
+            private static StringCalculator CreateStringCalculator()
+            {
+                //Arrange
+                return new StringCalculator();
+            }
+
+        }
+
+
+
+        // [Test]
+        // public void Leaning()
+        //{
+        //var exception = Assert.Throws<Exception>(() => int.Parse(""));
+        // Assert.AreEqual("Junk", exception.Message);
+        //Assert.AreEquals(true, string.IsNullOrEmpty("  "));
+        //}
+    }
+}
